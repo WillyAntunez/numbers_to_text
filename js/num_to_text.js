986c098abda51731179ab2cb3363610a5b17a240
Willy Antunez <willy.e.antunez@gmail.com>
2020-08-13 09:10:37 -0600

    Cree una mini base de datos de constantes con las nomenclaturas de los numeros

--- js/num_to_text.js
+++ js/num_to_text.js
@@ -1,5 +1,65 @@
 export function numberToTextES(number){
-    
+
+    const specialNumbers = {
+        'once': 11,
+        'doce': 12,
+        'trece': 12,
+        'catorce': 14,
+        'quince': 15,
+        'dieciséis': 16,	
+        'diecisiete':	17,
+        'dieciocho': 18,
+        'diecinueve':	19,
+        'veintiuno': 21,
+        'veintidós': 22,
+        'veintitrés': 23,
+        'veinticuatro': 24,
+        'veinticinco': 25,
+        'veintiséis': 26,
+        'veintisiete': 27,
+        'veintiocho': 28,
+        'veintinueve': 29
+    },
+        unitNumbers = {
+        'cero': 0,
+        'uno': 1,
+        'dos': 2,
+        'tres': 3,
+        'cuatro': 4,
+        'cinco': 5,
+        'seis': 6,
+        'siete': 7,
+        'ocho': 8,
+        'nueve': 9
+    },
+        tenNumbers = {
+        'diez':10,
+        'veinte': 20,
+        'treinta': 30, 
+        'cuarenta': 40, 
+        'cincuenta': 50, 
+        'sesenta': 60, 
+        'setenta': 70, 
+        'ochenta': 80, 
+        'noventa': 90
+    },
+        hundredNumbers = {
+        'cien': 100,
+        'doscientos': 200,
+        'trescientos': 300,
+        'cuatrocientos': 400,
+        'quinientos': 500,
+        'seiscientos': 600,
+        'setecientos': 700,
+        'ochocientos': 800,
+        'novecientos': 900
+    },
+        thousand = 1000,
+        million = 1000000;
+        
+
+
+
 }
 
 export function numberToTextEN(){
