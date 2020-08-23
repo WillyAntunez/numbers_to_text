8cdbf594e4235adf3dded06cc87f00a7d2ea44cb
Willy Antunez <willy.e.antunez@gmail.com>
2020-08-23 00:25:05 -0600

    Agregue manejo de errores en la validacion de datos.

--- js/index.js
+++ js/index.js
@@ -6,10 +6,31 @@ const $numberIn = document.getElementById('number-in'),
     $resultBtn = document.getElementById('result-btn'),
     $error = document.getElementById('error');
 
+let error = '';
+
 function validateNumber(num){
-    const isValid = /^-?[0-9]\d*(\.\d{0,12})?$/.test(num);
-    return isValid;
+    const isValid = /^-?\d{1,30}(\.\d{1,12})?$/.test(num);
+
+    if(isValid){
+        return true
+    }else{
+        if(/-/g.test(num) && (num.match(/-/g).length > 1 || /[0-9]-[0-9]?/.test(num))){
+            error = 'Introdujiste un signo negativo de manera incorrecta.';
+        }else if(/\./g.test(num) && (num.match(/\./g).length > 1 || !/\d\.\d/.test(num))){
+            error = 'Introdujiste un punto de manera incorrecta.';
+        }else if(/[0-9]?\.\d{12,}?$/.test(num)){
+            error = 'El limite de numeros decimales (despues del punto) es de doce digitos.'
+        }else if(/\d{30,}?\.?\d*?$/.test(num)){
+            error = 'El limite de numeros enteros (antes del punto) es de treinta digitos.'
+        }else{
+            error = 'El numero que has introducido no es valido.';
+        }
+        
+    }
+   
 };
+
+
 
 function writeResult(num){
     $resultOut.textContent = numberToText(num);
@@ -27,7 +48,7 @@ function getResult(number){
         $error.textContent = ''
     }else if(number != '' && !validateNumber(number)){
         hideResult();
-        $error.textContent = 'El numero que has introducido no es valido.'
+        $error.textContent = error;
     }else{
         hideResult();
         $error.textContent = '';
@@ -38,6 +59,7 @@ $numberIn.addEventListener('keyup', e => {
     let number = $numberIn.value;
     getResult(number);
 });
+
 
 $resultBtn.addEventListener('click', e=>{
     let number = $numberIn.value;
